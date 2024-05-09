8dd073f396f70719337225242f3e82f24a6262e1
Abdihafith <abdyhafith@gmail.com>
2024-05-09 18:23:12 +0300

    not passing still

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -1,11 +1,11 @@
 import {Link} from 'react-router-dom';
 
-function MovieCard({title}) {
-  
+function MovieCard({data}) {
   return (
     <article>
-        <h2>{title}</h2>
+        <h2>{data.title}</h2>
         {/* What should go here? */}
+        <Link to={`/movie/${data.id}`}>View Info</Link>
     </article>
   );
 };
